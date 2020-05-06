723c2aa52f31dd0c4790979b32581a5217f3b43e
ianstalter123 <istalter66@gmail.com>
2020-05-05 18:57:34 -0700

    Updates

---
+++ precache-manifest.c9d08826e81b8d474a803656ef1868cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6f8a0eea567015b6a953",
+    "url": "/portnew/static/css/main.4769dc47.chunk.css"
+  },
+  {
+    "revision": "6f8a0eea567015b6a953",
+    "url": "/portnew/static/js/main.6f8a0eea.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/portnew/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "f17be839b61a4b2f63fd",
+    "url": "/portnew/static/js/runtime~main.f17be839.js"
+  },
+  {
+    "revision": "7035af8cdbb03d49a16964b32bfdea7d",
+    "url": "/portnew/index.html"
+  }
+];
